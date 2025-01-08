6c849e914751d249bd77af06471d362bfaa28029
MahamMubashir <38991060+MahamMubashir@users.noreply.github.com>
2025-01-08 13:17:23 +0500

    NFT module upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Banner from "./components/Banner/Banner";
+
+function App() {
+  return (
+    <div className="main-container">
+      <Header />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,46 @@
+import star from "../../assets/baner1.png";
+import lines from "../../assets/banner2.png";
+import nft from "../../assets/banner3.png";
+import rocket from "../../assets/banner4.png";
+import elain from "../../assets/BannerEalain.png";
+
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      {/* Part 1: Ealain Logo */}
+      <div className="banner-logo">
+        <img src={elain} alt="Ealain Logo" />
+      </div>
+
+      {/* Part 2: Heading and Content */}
+      <div className="banner-content">
+        <h1 className="banner-heading">
+          CREATE <span className="highlight">NFT'S</span> OF{" "}
+          <span className="highlight">ART</span>
+        </h1>
+        <p className="banner-subtitle">NEW JOURNEY WITH 9,999 NFTs</p>
+        <p className="banner-description">
+          First NFT utility project for Developers and Designers based on
+        </p>
+        <div className="banner-buttons">
+          <button className="btn btn-primary">MINT NOW 🌟</button>
+          <button className="btn btn-secondary">JOIN US 🚀</button>
+        </div>
+      </div>
+
+      {/* Part 3: Images */}
+      <div className="banner-images">
+        <img src={star} alt="Star Icon" className="star-icon" />
+        <img src={lines} alt="Lines Icon" className="lines-icon" />
+        <div>
+          <img src={nft} alt="NFT Character" className="nft-character" />
+          <img src={rocket} alt="Rocket Icon" className="rocket-icon" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
